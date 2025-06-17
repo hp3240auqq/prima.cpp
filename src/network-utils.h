104e3b2356b88ceec2a533a8e0607c033f67687b
DeEMO <yzzxrx@foxmail.com>
2025-06-17 11:27:58 +0800

    fix: replace localhost to 127.0.0.1

--- src/network-utils.h
+++ src/network-utils.h
@@ -4,4 +4,4 @@
 
 typedef unsigned int uint32_t;
 
-bool isPortOpen(const std::string& ip, uint32_t port, int timeout_sec = 2);
+bool is_port_open(const std::string& ip, uint32_t port, int timeout_sec = 2);
